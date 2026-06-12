d0421ae13a6617c8f57cdb6200c50f4f771cf098
SriRepo-cloud <battinisrikanth96@gmail.com>
2026-06-12 20:45:27 +0530

    added a line inside practiceDSA()

--- src/main/java/com/javatechie/sms/api/Practice.java
+++ src/main/java/com/javatechie/sms/api/Practice.java
@@ -3,6 +3,7 @@ package com.javatechie.sms.api;
 public class Practice {
 
 	public void practiceDSA(String example) {
+		//Searching, Sorting, Graphs, Trees
 		System.out.println("Start practice on " + example);
 	}
 }
